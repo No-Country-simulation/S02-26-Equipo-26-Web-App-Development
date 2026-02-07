69c0b512d1611af1d3d546e4193d5cd425a7a78c
Marco Rodriguez <devmarcorodriguez@gmail.com>
2026-02-07 20:30:53 -0300

    entorno listo frontend react con vite y tailwind

---
+++ frontend-react-marco/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-red-600 flex items-center justify-center">
+      <p className="text-white text-4xl font-bold">
+        Marco
+      </p>
+    </div>
+  )
+}
