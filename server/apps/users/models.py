ec55e79ca24ac764112075fd9863c8dd5ca47463
C4le0 <frikositte@gmail.com>
2026-02-15 19:07:28 -0400

    feat: implement user, location, admin, patient, caregiver, and family models in the users app

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -1,12 +1,73 @@
 from django.db import models
 
-class UserRole(models.TextChoices):
-    ADMIN = "Admin", "Admin"
-    CAREGIVER = "Caregiver", "Caregiver"
-    PATIENT = "Patient", "Patient"
+
+class Location(models.Model):
+    location_id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'location'
+        app_label = 'users'
 
 
-role = models.CharField(
-    max_length=20,
-    choices=UserRole.choices
-)
+class User(models.Model):
+    # Requeridos por Django cuando AUTH_USER_MODEL apunta a este modelo
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "role"]
+
+    user_id = models.AutoField(primary_key=True)
+    location = models.ForeignKey(Location, models.DO_NOTHING, blank=True, null=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(unique=True, max_length=255)
+    password = models.CharField(max_length=225)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.TextField()  # TODO: Cambiar a choices con los ENUM
+    address_line = models.CharField(max_length=225, blank=True, null=True)
+    is_active = models.BooleanField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
+
+
+class Admin(models.Model):
+    admin = models.OneToOneField(User, models.DO_NOTHING, primary_key=True)
+    access_level = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'admin'
+
+
+class Patient(models.Model):
+    patient = models.OneToOneField(User, models.DO_NOTHING, primary_key=True)
+    medical_history = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'patient'
+
+
+class Caregiver(models.Model):
+    caregiver = models.OneToOneField(User, models.DO_NOTHING, primary_key=True)
+    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    bank_account = models.CharField(max_length=25, blank=True, null=True)
+    is_verified = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'caregiver'
+
+
+class Family(models.Model):
+    family_id = models.AutoField(primary_key=True)
+    patient = models.ForeignKey(Patient, models.DO_NOTHING, blank=True, null=True)
+    relationship = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'family'
